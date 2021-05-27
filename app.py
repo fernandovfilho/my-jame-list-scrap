3777c6c965aa3bf77b730c9c51c5309dacb2e1bf
Vinicius Matos <matosvinicius96@gmail.com>
2021-05-27 01:29:22 -0300

    'game' endpoint added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from requests import get
 from datetime import datetime
 app = Flask(__name__)
@@ -18,6 +18,7 @@ def homepage():
     for i in r['results']:
 
         name = i['name']
+        id = i['id']
         icon_url = i['image']['icon_url']
         thumb = i['image']['thumb_url']
         full_size = i['image']['original_url']
@@ -31,6 +32,7 @@ def homepage():
                 platforms.append(p['name'])
 
         data = {
+            'id': id,
             'nome': name,
             'imagens': {
                 'pequena': icon_url,
@@ -43,6 +45,15 @@ def homepage():
         games.append(data)
     
     return jsonify(games)
+
+@app.route('/game')
+def seachById():
+    game_id = request.args.get('id')
+    if game_id is None:
+        return "paramentro 'query' não especificado"
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    r = get(f"https://www.giantbomb.com/api/game/{game_id}/?api_key=b6a60d7da93c2fc2cb3d5256ac814ff324be71eb&format=json", headers=headers).json()
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
